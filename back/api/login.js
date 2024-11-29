b3c8953f04d2afdc90e2d5e00a5fa087c85c2722
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-11-29 23:56:08 +0900

    とりあえずAPI用ファイル分割

---
+++ back/api/login.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+const router = express.Router();
+
+
+
+async function postlogin(req, res){
+}
+router.post('/',postlogin);
+
+async function postsignup(req, res){
+}
+router.post('/signup',postsignup);
+
+async function postloginmail(req, res){
+}
+router.post('/mail',postloginmail);
+
+
+
+module.exports = router;
