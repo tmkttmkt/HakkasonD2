28b7808b6c87016512ea1e7afd9ccef10a2a5535
taketake-dev <taketake25121@gmail.com>
2024-11-30 23:02:54 +0900

    ローカルじゃなくした

--- back/api/index.js
+++ back/api/index.js
@@ -1,19 +1,12 @@
-// index.js
 const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
-const port = 3000;
 
 // ルートエンドポイントにアクセスしたときに「Hello World」を表示
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// サーバーの起動
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-
 // サーバーレス関数としてExpressをエクスポート
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
