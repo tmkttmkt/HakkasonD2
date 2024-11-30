8c52c96bca7ace26401da02c735c73e2041b8e61
taketake-dev <taketake25121@gmail.com>
2024-11-30 23:10:08 +0900

    portに変更

--- back/api/index.js
+++ back/api/index.js
@@ -3,10 +3,14 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+const port = 5000;
+
 // ルートエンドポイントにアクセスしたときに「Hello World」を表示
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// サーバーレス関数としてExpressをエクスポート
-module.exports = serverless(app);
+// サーバーの起動
+app.listen(port, () => {
+    console.log(`Server is running at http:localhost:${port}`);
+});
