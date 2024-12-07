2533a03912fe6706a99df6a49b0c7c7bfe4a66eb
TMKT <126658967+tmkttmkt@users.noreply.github.com>
2024-12-07 17:55:08 +0900

    ｇれｗｖさげわげｗｈれｔｙｒｍｋｐべｄ

--- back/api/aws_wrapper.js
+++ back/api/aws_wrapper.js
@@ -1,5 +1,22 @@
 require('dotenv').config();
-const { DynamoDBClient, PutItemCommandInput, PutItemCommand  } = require("@aws-sdk/client-dynamodb");
+const {
+  BillingMode,
+  CreateTableCommand,
+  DeleteTableCommand,
+  DynamoDBClient,
+  waitUntilTableExists,
+  PutItemCommand,
+} =require("@aws-sdk/client-dynamodb");
+const {
+  BatchWriteCommand,
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  paginateQuery,
+  paginateScan,
+} = require("@aws-sdk/lib-dynamodb");
 
 const dynamodbClient = new DynamoDBClient({
   region: process.env.AWS_REGION, // 環境変数からリージョンを取得
@@ -10,22 +27,67 @@ const dynamodbClient = new DynamoDBClient({
   },
   endpoint: undefined
 });
-const testdo = async (event)=> {
+const docClient = DynamoDBDocumentClient.from(dynamodbClient);
+/*const testdo = async (event)=> {
   const params= {
       TableName: 'test222222',
       Item: {
-          user_id: { S: '1' },
-          name: { S: 'John' },
-          age: { N: '20' },
+          user_id: { S: '3' },
+          name: { S: 'Jo2323dd' },
+          age: { N: '203' },
       },
   };
   const command = new PutItemCommand(params);
   try {
       await dynamodbClient.send(command);
+      console.log("\nrrr-put\n");
   } catch (err) {
     console.log(err)
   }
 };
 
-testdo();
+testdo();*/
+/*
+const testget = async (event)=> {
+  const params = {
+    TableName: "test222222",
+    Key: { user_id: "2"  },
+  };
+  const command = new GetCommand(params);
+  try {
+      const a=await docClient.send(command);
+      console.log(a.Item);
+  } catch (err) {
+    console.log(err)
+  }
+}
+testget();//*/
+/*
+const testgets = async (event)=> {
+  const paginator = paginateScan({ client: docClient }, { TableName: "test222222" });
+  for await (const page of paginator) {
+    console.log(page.Items);
+    console.log("\nrrr-gets\n");
+  }
+}
+testgets();//*/
+
+const testdel = async (event)=> {
+  const params = {
+    TableName: "test222222",
+    Key: {
+      user_id: "2",  // テーブルのキーを指定
+    }
+  };
+
+  const command = new DeleteCommand(params);
+
+  try {
+    const data = await docClient.send(command);
+    console.log("Item deleted:", data);
+  } catch (err) {
+    console.error("Error deleting item:", err);
+  }
+}
+testdel();//*/
 module.exports=dynamodbClient
